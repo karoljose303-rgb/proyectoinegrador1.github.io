d5d835967d10d8dee4744412a9b0a748bbbdd344
karoljose303-rgb <karoljose303@gmail.com>
2026-07-04 01:32:23 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$servidor = "localhost";
-$usuarioBD = "root";
-$passwordBD = "";
-$baseDatos = "proyecto";
+$servidor = "sql5.freesqldatabase.com";
+$usuarioBD = "sql5832235";
+$passwordBD = "XHzwpw1gUJ";
+$baseDatos = " sql5832235";
 
 $conexion = new mysqli($servidor, $usuarioBD, $passwordBD, $baseDatos);
 
